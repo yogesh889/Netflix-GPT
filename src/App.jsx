0f749db1ad574729631c034d37d5ad302021a035
Yogesh889 <8058yogesh@gmail.com>
2025-04-20 20:44:41 +0530

    Setup CRA & Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-const App = () => {
+import React from 'react';
+import Header from "./Header"
+
+function App() {
   return (
     <div>
-      <h1>hello</h1>
+      <h1 className='text-5xl font-bold'>Hello React!</h1>
+      <Header />
     </div>
-  )
+  );
 }
 
 export default App;
